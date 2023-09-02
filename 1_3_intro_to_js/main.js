558cefc680f5a8d58ff9dced85dae1b38a2c7407
Josh Hadro <josh.hadro@gmail.com>
2023-09-01 22:23:39 -0400

    First pass at 1_3 exercise

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// console.log('hello world');
+
+var i = 1;
+
+var counter = document.getElementById("counter").innerText;
+
+// console.log(counter);
+
+updateCounter = () => {
+    message = (i == 1) ? "1 time you've said you love Beeker" : i + " times you've said you love Beeker";
+    document.getElementById("counter").innerText = message;
+    // console.log(i, counter);
+    i++;
+  }
+
+window.onclick = updateCounter;
